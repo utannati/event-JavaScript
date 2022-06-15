c8079ea2c93db875b95ac7924d99fea3d95d5903
Natnael Tekle <102483031+utannati@users.noreply.github.com>
2022-06-14 20:27:10 -0400

    event

---
+++ Main.js
@@ -0,0 +1,9 @@
+var name = "JavaScript!";
+alert(name)
+var btn = document.getElementById("btn");
+function displayDate() {
+    document.getElementById("Tue Jun 14 2022 16:28:23 GMT-0400 (Eastern Daylight Time)").innerHTML = Date();
+  }
+
+button = onclick="displayDate(The Time is?)"
+
